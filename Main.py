627a23fa8b47fe29c7f921414f5cc1f1ad851f4d
James Clarke <jamesclarke@Jamess-MacBook-Pro-6.local>
2025-10-10 15:18:02 -0700

    small fix with the location of the birds the bird pictures are inside of the test folder and not the birds folder

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import os
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
-Image_Folder = r"Birds"
+Image_Folder = r"Birds/test"
 for filename in os.listdir(Image_Folder):
     if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
         image_path = os.path.join(Image_Folder, filename)
@@ -14,5 +14,4 @@ for filename in os.listdir(Image_Folder):
         inputs = processor(image, return_tensors="pt")
         outputs = model.generate(**inputs)
         captions = processor.decode(outputs[0], skip_special_tokens = True)
-
-print(f"{filename} : {captions}")
+        print(f"{filename} : {captions}")
