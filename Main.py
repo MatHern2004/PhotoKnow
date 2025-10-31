6a74993b21c0157cfaaaa088cb623a12b9dcdba4
MatHern2004 <mhernandez3142@sdsu.edu>
2025-10-31 14:21:26 -0700

    Streamlit displays birds

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 from transformers import BlipProcessor, BlipForConditionalGeneration
+import streamlit as st
 from PIL import Image
-import torch
 import os
 
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
-Image_Folder = r"Birds/test"
+Image_Folder = r"Birds/test" # Retrieve the image folder
+placeholder = st.empty()
 for filename in os.listdir(Image_Folder):
     if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
         image_path = os.path.join(Image_Folder, filename)
@@ -15,3 +16,7 @@ for filename in os.listdir(Image_Folder):
         outputs = model.generate(**inputs)
         captions = processor.decode(outputs[0], skip_special_tokens = True)
         print(f"{filename} : {captions}")
+        placeholder.image(image_path, caption=captions)
+        
+                
+
